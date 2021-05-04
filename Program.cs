69920123c7c70125f27cdd2288821dd1b7d88d95
Mercuryrus <83664861+Mercuryrus@users.noreply.github.com>
2021-05-04 20:22:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            double z1, z2;
+            Console.Write("Введите параметр а: ");
+            double a = double.Parse(Console.ReadLine());
+            
+            z1 = (Math.Sin((Math.PI / 2.0) + (3 * a))) / (1 - Math.Sin((3 * a) - Math.PI));
+            z2 = 1.0 / Math.Tan(((5.0 / 4.0) * Math.PI) + ((3.0 / 2.0) * a));
+
+            Console.WriteLine("z1 = {0} \nz2 = {1}", z1, z2);
+
+            Console.ReadKey();
+        }
+    }
+}
